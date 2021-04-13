f05bb49679d1a3b79cf70740b0e7d048156a3957
Mateus Vitor <44790156+Stanniack@users.noreply.github.com>
2021-04-13 07:55:26 -0300

    Parâmetros Json por Express
    requisitando parâmetros http.

--- expressJS/appexpress.js
+++ expressJS/appexpress.js
@@ -3,7 +3,11 @@ const app = express()
 const port = 8081
 
 app.get("/", function(request, answer) {
-    answer.send('Primeira págins com express.')
+    answer.send('Primeira página com express.')
+})
+
+app.get("/sobre/:nome/:idade", function(request, answer) {
+    answer.send(request.params.nome)
 })
 
 app.listen(port, function() {
