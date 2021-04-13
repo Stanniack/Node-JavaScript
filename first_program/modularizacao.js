25e762963d07cc2fa0936b19827101a281c0be6d
Mateus Vitor <44790156+Stanniack@users.noreply.github.com>
2021-04-13 05:43:32 -0300

    Modularização
    Algumas formas de modularizar o código com NodeJS

---
+++ first_program/modularizacao.js
@@ -0,0 +1,5 @@
+var sum = function (number1, number2) {
+    return number1 + number2
+}
+
+export default sum
---
+++ first_program/operation.js
@@ -0,0 +1,5 @@
+//import sub_sum from "./operation.js"
+//import Person from "./class.js"
+
+let person = require("./class")
+console.log(new person('Mateus').toString())
