5f79a60f99bd9e350c9354337551cbf5dd6a74c8
Mateus Vitor <44790156+Stanniack@users.noreply.github.com>
2021-04-13 08:06:45 -0300

    Integrando HTML com Express

--- expressJS/appexpress.js
+++ expressJS/appexpress.js
@@ -3,13 +3,15 @@ const app = express()
 const port = 8081
 
 app.get("/", function(request, answer) {
-    answer.send('Primeira página com express.')
+    // dirname é o diretório raiz
+    answer.sendFile(__dirname + "/html/index.html")
 })
 
 app.get("/sobre/:nome/:idade", function(request, answer) {
     answer.send(request.params.nome)
 })
 
+/* Inicialização do servidor */
 app.listen(port, function() {
     console.log(`Servidor rodando na porta ${port}`)
 })
