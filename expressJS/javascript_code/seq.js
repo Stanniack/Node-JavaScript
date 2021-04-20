6bc35b4f10c39874c4e473d581abbcdc3553b820
Mateus Vitor <44790156+Stanniack@users.noreply.github.com>
2021-04-20 04:00:49 -0300

    Adicionando comentários

--- expressJS/javascript_code/seq.js
+++ expressJS/javascript_code/seq.js
@@ -1,10 +1,13 @@
 const Sequelize = require('sequelize')
+
+/* Conexão do bd: nomeBanco, nomeUser, senha, json de configuração */
 const sequelize = new Sequelize('test', 'root', '', {
     host: 'localhost',
     dialect: 'mysql'
 
 })
 
+/* Tratamento de exceção p verificar se o banco foi conectado com sucesso */
 sequelize.authenticate().then(function () {
     console.log('Conectado com sucesso!')
 }).catch(function (err) {
@@ -12,6 +15,7 @@ sequelize.authenticate().then(function () {
 })
 
 /* Criando Tabela/Models */
+/* nomeTabela, json com os atributos e suas configurações */
 const Post = sequelize.define('post', {
     titulo: {
         type: Sequelize.STRING
