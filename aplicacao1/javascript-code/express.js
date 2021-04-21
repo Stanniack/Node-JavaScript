40f390917ed4c10542323f686d34916a1cb34081
Mateus Vitor <44790156+Stanniack@users.noreply.github.com>
2021-04-21 03:28:29 -0300

    Configurando handlebars

--- aplicacao1/javascript-code/express.js
+++ aplicacao1/javascript-code/express.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const app = express()
+const handlebars = require('express-handlebars')
 const port = 8081
 
+/*Configurações básicas da aplicação */
 
+    /* configurando template engine: templateEngine, página html default */
+    app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+    app.set('view engine', 'handlebars')
+
+
+/* Roda o express */
 app.listen(port, function () {
     console.log('Servidor rodando na porta: ' + port)
 })
