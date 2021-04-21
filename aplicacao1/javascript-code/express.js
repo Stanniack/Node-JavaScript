16bf248b191932d6e82640f4894b9edf2b09cb76
Mateus Vitor <44790156+Stanniack@users.noreply.github.com>
2021-04-21 03:45:16 -0300

    Configurando Sequelize e MySql

--- aplicacao1/javascript-code/express.js
+++ aplicacao1/javascript-code/express.js
@@ -1,6 +1,8 @@
 const express = require('express')
-const app = express()
+const {Sequelize} = require('sequelize')
 const handlebars = require('express-handlebars')
+
+const app = express()
 const port = 8081
 
 /*Configurações básicas da aplicação */
@@ -9,8 +11,21 @@ const port = 8081
     app.engine('handlebars', handlebars({defaultLayout: 'main'}))
     app.set('view engine', 'handlebars')
 
+    /* Configurando banco de dados */
+    const sequelize = new Sequelize('test', 'root', '', {
+        host: 'localhost',
+        dialect: 'mysql'
+    })
+
 
 /* Roda o express */
 app.listen(port, function () {
     console.log('Servidor rodando na porta: ' + port)
+})
+
+/* Verifica banco de dados */
+sequelize.authenticate().then(function() {
+    console.log('MySQL Rodando')
+}).catch(function(err) {
+    console.log('Erro ao inicializar Mysql: ' + err)
 })
