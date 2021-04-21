134d950ac1c6d075c78d48d39704c8fb3894bcc0
Mateus Vitor <44790156+Stanniack@users.noreply.github.com>
2021-04-21 13:24:13 -0300

    Modularizando aplicação
    Criando pasta models para criar os modelos e inicializar a database

--- aplicacao1/javascript-code/express.js
+++ aplicacao1/javascript-code/express.js
@@ -1,10 +1,11 @@
 const express = require('express')
-const {Sequelize} = require('sequelize')
 const handlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
 
 const app = express()
 const port = 8081
+
+// -----------------------------------
 
 /* Configurações básicas da aplicação */
 
@@ -12,34 +13,26 @@ const port = 8081
     app.engine('handlebars', handlebars({defaultLayout: 'main'}))
     app.set('view engine', 'handlebars')
 
-    /* Configurando banco de dados */
-    const sequelize = new Sequelize('test', 'root', '', {
-        host: 'localhost',
-        dialect: 'mysql'
-    })
-
     /* Configurando Body-parser (conversor de requisições) */
     app.use(express.urlencoded({extended: false}))
     app.use(express.json())
 
+// -----------------------------------
 
-/* Rotas express */
-app.get('/cadastro', function(requestion, answer){
-    answer.render('form')
-}) 
+    /* Rotas express */
+    app.get('/cadastro', function(requestion, answer){
+        answer.render('form')
+    }) 
 
-app.post('/app', function(requestion, answer) {
-    answer.send('Título: ' + requestion.body.titulo)
-})
+    app.post('/app', function(requestion, answer) {
+        answer.send('Título: ' + requestion.body.titulo)
+    })
+
+
+// -----------------------------------
 
 /* Roda o express */
 app.listen(port, function () {
     console.log('Servidor rodando na porta: ' + port)
 })
 
-/* Verifica banco de dados */
-sequelize.authenticate().then(function() {
-    console.log('MySQL Rodando')
-}).catch(function(err) {
-    console.log('Erro ao inicializar Mysql: ' + err)
-})
---
+++ aplicacao1/models/database.js
@@ -0,0 +1,19 @@
+const {Sequelize} = require('sequelize')
+
+/* Configurando banco de dados */
+const sequelize = new Sequelize('test', 'root', '', {
+    host: 'localhost',
+    dialect: 'mysql'
+})
+
+/* Verifica banco de dados */
+sequelize.authenticate().then(function() {
+    console.log('MySQL Rodando')
+}).catch(function(err) {
+    console.log('Erro ao inicializar Mysql: ' + err)
+})
+
+module.exports = {
+    Sequelize: Sequelize,
+    sequelize: sequelize
+}
